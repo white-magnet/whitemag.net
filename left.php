a8e8db37ecb38711c17b7959e175bf1ad27fd82f
Kulbir Saini <saini@saini.co.in>
2011-12-24 20:15:05 +0530

    Added copyright notice and twitter link.

--- left.php
+++ left.php
@@ -1,11 +1,15 @@
         <div class="section-left left">
           <h1>WELCOME</h1>
           <ul id="menu" class="left">
-          <li><a href="./index.php" <?php if ($cur == 'home') { print 'class="on"'; } ?>>Home</a></li>
-            <li><a href="./about.php" <?php if ($cur == 'about') { print 'class="on"'; } ?>>About</a></li>
+            <li><a href="./index.php" <?php if ($cur == 'home') { print 'class="on"'; } ?>>Home</a></li>
+            <!--li><a href="./about.php" <?php if ($cur == 'about') { print 'class="on"'; } ?>>About</a></li-->
             <li><a href="./products.php" <?php if ($cur == 'products') { print 'class="on"'; } ?>>Products</a></li>
             <li><a href="./team.php" <?php if ($cur == 'team') { print 'class="on"'; } ?>>Team</a></li>
             <li><a href="./contact.php" <?php if ($cur == 'contact') { print 'class="on"'; } ?>>Contact</a></li>
           </ul><!-- //menu -->
+          <div class="clear"></div>
+          <div class="social">
+            <a href="http://twitter.com/white_magnet" target="_blank" rel="nofollow" class="link">@white_magnet</a>
+          </div>
         </div><!--section-left-->
         <div class="line left-line left"></div>
--- right.php
+++ right.php
@@ -2,3 +2,6 @@
         <div class="section-right right">
           <a href="./" id="logo" class="right"></a>
         </div><!--section-right-->
+        <div class="copyright left">
+          <p class="text-center">&copy 2011 White Magnet Software Pvt Ltd. All rights reserved.</p>
+        </div>
