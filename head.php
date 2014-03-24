75d1458d3140c6c7a40dd302b2e0284788639162
Kulbir Saini <saini@saini.co.in>
2014-03-24 22:17:01 +0530

    Updated site with new videocache logo

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="description" content="Dynamic video caching solutions using Squid Proxy Server for small and medium scale networks." />
     <meta name="keywords" content="White Magnet, White Magnet Software, WhiteMagnet, Cache Youtube, Cache Youtube videos, Videocache, Cache Videos, Proxy Server, Video Acceleration, Video Proxy, Squid Server, Squid Consultancy, Squid Support, Squid" />
-    <link rel="stylesheet" href="/css/style.css" type="text/css" media="screen, projection" />
+    <link rel="stylesheet" href="./css/style.css" type="text/css" media="screen, projection" />
     <script type="text/javascript" src="http://use.typekit.com/woq2ogg.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
--- left.php
+++ left.php
@@ -1,10 +1,10 @@
         <div class="section-left left">
           <ul id="menu" class="left">
-            <li><a href="/" <?php if ($cur == 'home') { print 'class="on"'; } ?>>Home</a></li>
+            <li><a href="./" <?php if ($cur == 'home') { print 'class="on"'; } ?>>Home</a></li>
             <!--li><a href="./about.php" <?php if ($cur == 'about') { print 'class="on"'; } ?>>About</a></li-->
-            <li><a href="/products.php" <?php if ($cur == 'products') { print 'class="on"'; } ?>>Products</a></li>
-            <li><a href="/team.php" <?php if ($cur == 'team') { print 'class="on"'; } ?>>Team</a></li>
-            <li><a href="/contact.php" <?php if ($cur == 'contact') { print 'class="on"'; } ?>>Contact</a></li>
+            <li><a href="./products.php" <?php if ($cur == 'products') { print 'class="on"'; } ?>>Products</a></li>
+            <li><a href="./team.php" <?php if ($cur == 'team') { print 'class="on"'; } ?>>Team</a></li>
+            <li><a href="./contact.php" <?php if ($cur == 'contact') { print 'class="on"'; } ?>>Contact</a></li>
           </ul><!-- //menu -->
           <div class="clear"></div>
         </div><!--section-left-->
